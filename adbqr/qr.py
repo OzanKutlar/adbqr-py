d47da1519760845a7ce665a3e289dfd234f2d513
Ozan Kutlar <ozankutlar@protonmail.com>
2026-05-15 15:08:35 +0300

    fix(qr): add thick white quiet zone and revert to robust half-block rendering

--- adbqr/qr.py
+++ adbqr/qr.py
@@ -6,7 +6,7 @@ class QrRenderer:
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=1,
-            border=4,
+            border=2,  # Minimal internal border, we will add massive manual padding
         )
         qr.add_data(data)
         qr.make(fit=True)
@@ -15,16 +15,38 @@ class QrRenderer:
         self.height = len(self.matrix)
 
     def render(self):
+        # Force Black Foreground (30) and White Background (47)
+        COLOR = "\x1b[30;47m"
+        RESET = "\x1b[0m"
+
+        # 6 spaces of pure white padding on left and right
+        pad_x = "      "
+        blank_line = COLOR + pad_x + (" " * self.width) + pad_x + RESET
+
         print()
-        for y in range(self.height):
-            row = "  " # Left indent for visual padding
+        # Top quiet zone
+        print(blank_line)
+        print(blank_line)
+
+        for y in range(0, self.height, 2):
+            row_str = COLOR + pad_x
             for x in range(self.width):
-                if self.matrix[y][x]:
-                    # Black background, two spaces wide for square aspect ratio
-                    row += "\x1b[40m  "
+                top = self.matrix[y][x]
+                bottom = self.matrix[y+1][x] if (y+1) < self.height else False
+
+                # Use half-blocks to ensure perfect square aspect ratio
+                if top and bottom:
+                    row_str += '█'
+                elif top and not bottom:
+                    row_str += '▀'
+                elif not top and bottom:
+                    row_str += '▄'
                 else:
-                    # White background, two spaces wide
-                    row += "\x1b[47m  "
-            row += "\x1b[0m" # Reset color at end of line
-            print(row)
+                    row_str += ' '
+            row_str += pad_x + RESET
+            print(row_str)
+
+        # Bottom quiet zone
+        print(blank_line)
+        print(blank_line)
         print()
